6e125811a5650bdf36dee9ecdc37e5f4c6251c4b
Ulric Wilfred <shaman.sir@gmail.com>
2017-11-16 19:12:09 +0100

    treat Patch as a Signal with Nodes, just like in Flare (just a draft)

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,17 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Signal as S
+import Signal.Channel (CHANNEL, subscribe, send, channel)
+
+-- | A `Patch` is a `Signal` with a corresponding list of HTML elements
+-- | for the user interface components.
+data Patch a = Patch (Array Node) (S.Signal a)
+
+-- | A `Node` is a `Signal` with a corresponding list of HTML elements
+-- | for the user interface components.
+data Node a = Node (Array Element) (S.Signal a)
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
