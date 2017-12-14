3c6e0e5fb4c90f13fa9a8a3827b641172e2f709d
Ulric Wilfred <shaman.sir@gmail.com>
2017-12-14 19:48:30 +0100

    make node/channel types parametric, so user will be able to define own types for them

--- src/Main.purs
+++ src/Main.purs
@@ -11,48 +11,51 @@ import Signal.Time as ST
 
 type Id = String
 
-data Type = Tuple String String
+-- `n` — node type
+-- `c` — channel type
+-- `a` — data type
+-- `x` — error type
 
-type PatchModel a x =
+type PatchModel n c a x =
     { id :: Id
     , title :: String
-    , nodes :: Array (Node a x)
-    , links :: Array (Link a x)
+    , nodes :: Array (Node n c a x)
+    , links :: Array (Link c a x)
     }
 
-type NodeModel =
+type NodeModel n c =
     { id :: Id
     , title :: String
-    , type :: Type
-    , inlets :: Array InletModel
-    , outlets :: Array OutletModel
+    , type :: n
+    , inlets :: Array (InletModel c)
+    , outlets :: Array (OutletModel c)
     }
 
-type InletModel =
+type InletModel c =
     { id :: Id
     , label :: String
-    , type :: Type
+    , type :: c
     }
 
-type OutletModel =
+type OutletModel c =
     { id :: Id
     , label :: String
-    , type :: Type
+    , type :: c
     }
 
 data Flow a x = Bang | Data a | Error x
 
-data Patch a x = Patch (PatchModel a x) (S.Signal (Flow a x))
+data Patch n c a x = Patch (PatchModel n c a x) (S.Signal (Flow a x))
 
-data Node a x = Node NodeModel (S.Signal (Flow a x))
+data Node n c a x = Node (NodeModel n c) (S.Signal (Flow a x))
 
-data Inlet a x = Inlet InletModel (S.Signal (Flow a x))
+data Inlet c a x = Inlet (InletModel c) (S.Signal (Flow a x))
 
-data Outlet a x = Outlet OutletModel (S.Signal (Flow a x))
+data Outlet c a x = Outlet (OutletModel c) (S.Signal (Flow a x))
 
-data Link a x = Link (Outlet a x) (Inlet a x)
+data Link c a x = Link (Outlet c a x) (Inlet c a x)
 
-createPatch :: forall a x. String -> Patch a x
+createPatch :: forall n c a x. String -> Patch n c a x
 createPatch title =
     Patch
         { id : "test"
@@ -62,7 +65,7 @@ createPatch title =
         }
         (S.constant Bang)
 
-createNode :: forall a x. String -> Type -> Node a x
+createNode :: forall n c a x. String -> n -> Node n c a x
 createNode title nodeType =
     Node
         { id : "test"
@@ -73,7 +76,7 @@ createNode title nodeType =
         }
         (S.constant Bang)
 
-createInlet :: forall a x. String -> Type -> Inlet a x
+createInlet :: forall c a x. String -> c -> Inlet c a x
 createInlet label inletType =
     Inlet
         { id : "test"
@@ -82,7 +85,7 @@ createInlet label inletType =
         }
         (S.constant Bang)
 
-createOutlet :: forall a x. String -> Type -> Outlet a x
+createOutlet :: forall c a x. String -> c -> Outlet c a x
 createOutlet label outletType =
     Outlet
         { id : "test"
@@ -91,17 +94,17 @@ createOutlet label outletType =
         }
         (S.constant Bang)
 
-connect :: forall a x. Outlet a x -> Inlet a x -> Link a x
+connect :: forall c a x. Outlet c a x -> Inlet c a x -> Link c a x
 connect outlet inlet =
     Link outlet inlet
 
-addInlet :: forall a x. Inlet a x -> Node a x -> Node a x
+addInlet :: forall n c a x. Inlet c a x -> Node n c a x -> Node n c a x
 -- addInlet inlet'@(Inlet inlet inletSignal) (Node node nodeSignal) =
 --  Node (node { inlets = inlet' : node.inlets }) (S.merge nodeSignal inletSignal)
 addInlet (Inlet inlet inletSignal) (Node node nodeSignal) =
     Node (node { inlets = inlet : node.inlets }) (S.merge nodeSignal inletSignal)
 
-addOutlet :: forall a x. Outlet a x -> Node a x -> Node a x
+addOutlet :: forall n c a x. Outlet c a x -> Node n c a x -> Node n c a x
 addOutlet (Outlet outlet outletSignal) (Node node nodeSignal) =
     Node (node { outlets = outlet : node.outlets }) (S.merge nodeSignal outletSignal)
 
