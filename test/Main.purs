59f89852c0531f238e88d79e3393a93eee34091c
Ulric Wilfred <shaman.sir@gmail.com>
2018-02-01 22:14:36 +0100

    add the ability to write specs for signals with purs-spec, using purs-unit-tests from purs-signal as a reference

--- test/Main.purs
+++ test/Main.purs
@@ -4,24 +4,25 @@ import Prelude
 
 import Data.Time.Duration as Duration
 
-import Control.Monad.Aff (delay)
+import Control.Monad.Aff (Aff, delay, forkAff)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.AVar (AVAR)
-
-import Test.Unit (suite, test)
-import Test.Unit.Assert as Assert
-import Test.Unit.Console (TESTOUTPUT)
-import Test.Unit.Main (runTest)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Ref (REF)
 
 import Test.Spec (pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
 
+import Signal as S
+import Signal.Channel as SC
+import Signal.Time (debounce)
+
+import Test.Signal (expect')
+
 --import Rpd
 
-main :: Eff (RunnerEffects ()) Unit
+main :: forall eff. Eff (RunnerEffects ( ref :: REF, channel :: SC.CHANNEL | eff )) Unit
 main = run [consoleReporter] do
   describe "purescript-spec" do
     describe "Attributes" do
@@ -38,23 +39,26 @@ main = run [consoleReporter] do
         res <- pure "Alligator"
         res `shouldEqual` "Alligator"
       it "is PureScript 0.10.x compatible" $ pure unit
+      it "tests signals"
+        $ expect' (S.constant "lol") ["lol"]
+      it "tests async signals" $ do
+        chan <- liftEff $ SC.channel 0
+        let sig = debounce 10.0 $ SC.subscribe chan
+            send' = liftEff <<< SC.send chan
 
--- main :: forall eff. Eff ( console :: CONSOLE
---                         , testOutput :: TESTOUTPUT
---                         , avar :: AVAR
---                         | eff) Unit
--- main = runTest do
---   suite "API" do
---     test "initialization" do
---       Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
---       Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
---       Assert.equal (2 + 2) 4
---       Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal (2 + 2) 5
-  -- suite "async code" do
-  --   test "with async IO" do
-  --     fileContents <- FS.readTextFile UTF8 "file.txt"
-  --     Assert.equal fileContents "hello here are your file contents"
-  --   test "async operation with a timeout" do
-  --     timeout 100 $ do
-  --       file2Contents <- FS.readTextFile UTF8 "file2.txt"
-  --       Assert.equal file2Contents "can we read a file in 100ms?"
+        _ <- forkAff $ expect' sig [0,2,4]
+        wait 20.0
+        send' 1
+        wait 5.0
+        send' 2
+        wait 20.0
+        send' 3
+        wait 5.0
+        send' 4
+        wait 20.0
+
+
+wait :: forall e. Number -> Aff e Unit
+wait t = do
+  delay (Duration.Milliseconds t)
+  pure unit
--- test/Signal.purs
+++ test/Signal.purs
@@ -1,25 +1,17 @@
-module Test.Signal where
-  -- ( tick
-  -- ( expect
-  -- , expectFn
-  -- , tick
-  -- ) where
+module Test.Signal
+  ( expect'
+  ) where
 
 import Prelude
-import Control.Monad.Aff (makeAff, nonCanceler)
-import Control.Monad.Aff.AVar (AVAR)
-import Control.Monad.Eff (Eff)
+import Control.Monad.Aff (Aff, makeAff, nonCanceler)
 import Control.Monad.Eff.Exception (error)
 import Control.Monad.Eff.Ref (REF, writeRef, readRef, newRef)
-import Control.Monad.Eff.Timer (TIMER)
 import Data.Either (Either(..))
-import Data.Function.Uncurried (Fn4, runFn4)
 import Data.List (List(..), fromFoldable, toUnfoldable)
-import Signal (Signal, constant, (~>), runSignal)
-import Test.Spec.Runner (RunnerEffects, run, run')
+import Signal (Signal, (~>), runSignal)
 
-expectFn :: forall e a. Eq a => Show a => Signal a -> Array a -> Eff (ref :: REF | e) Unit
-expectFn sig vals = makeAff \resolve -> do
+expect' :: forall e a. Eq a => Show a => Signal a -> Array a -> Aff (ref :: REF | e) Unit
+expect' sig vals = makeAff \resolve -> do
   remaining <- newRef vals
   let getNext val = do
         nextValArray <- readRef remaining
@@ -33,11 +25,3 @@ expectFn sig vals = makeAff \resolve -> do
           Nil -> resolve $ Left $ error "unexpected emptiness"
   runSignal $ sig ~> getNext
   pure nonCanceler
-
-expect :: forall e a. Eq a => Show a => Int -> Signal a -> Array a -> Eff (ref :: REF, timer :: TIMER, avar :: AVAR | e)
-expect time sig vals = run' { timeout : time } $ expectFn sig vals
-
--- foreign import tickP :: forall a c. Fn4 (c -> Signal c) Int Int (Array a) (Signal a)
-
--- tick :: forall a. Int -> Int -> Array a -> Signal a
--- tick = runFn4 tickP constant
