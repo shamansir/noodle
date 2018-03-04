da0705ff21f5c3e3dc5f3b67a58bb7fd5ae8c78d
Ulric Wilfred <shaman.sir@gmail.com>
2018-03-04 23:12:09 +0100

    put new API into Main.purs with using Smolder to render some stuff

--- src/Main.purs
+++ src/Main.purs
@@ -6,9 +6,11 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console as C
 
 import Rpd as Rpd
+import Signal as S
 import Signal.Channel as SC
 
-import Data.Foldable (for_)
+import Data.Foldable (for_, fold)
+import Data.Tuple
 import DOM (DOM)
 import DOM.HTML (window)
 import DOM.HTML.Types (htmlDocumentToNonElementParentNode)
@@ -17,14 +19,76 @@ import DOM.Node.NonElementParentNode (getElementById)
 import DOM.Node.Types (ElementId(..))
 import Text.Smolder.Renderer.DOM (render)
 
-import Text.Smolder.Markup (Markup, text) as H
-import Text.Smolder.HTML (div) as H
+import Text.Smolder.Markup as H
+import Text.Smolder.HTML as H
 
 
-view :: ∀ e. H.Markup e
-view =
+data MyData
+  = Bang
+  | Str' String
+  | Int' Int
+
+
+type ProcessF d = (Array (Tuple String d) -> Array (Tuple String d))
+
+type AdaptF d = (d -> d)
+
+data Network d = Network (Array (Patch d))
+data Patch d = Patch String (Array (Node d)) (Array Link)
+--data Patch = Patch String (Array (Node Unit Unit)) (Array Link)
+data Node d = Node String (Array (Inlet d)) (Array (Outlet d)) (ProcessF d) -- (S.Signal Unit)
+--data Node a b = Node String (a -> b)
+--data Node a b = Node String (Map String Unit -> Map String Unit)
+data Inlet d = Inlet String (S.Signal d)
+--data Inlet d = Inlet String (Maybe (AdaptF d))
+data Outlet d = Outlet String (S.Signal d)
+data Link = Link
+
+
+myNode =
+  Node "f"
+    [ Inlet "a" (S.constant (Str' "i"))
+    , Inlet "b" (S.constant (Int' 2))
+    ]
+    [ Outlet "c" (S.constant (Int' 6))
+    ]
+    (\_ -> [ (Tuple "c" (Int' 10) ) ] )
+
+
+--myNetwork :: Network MyData
+myNetwork =
+  Network
+    [ Patch "p"
+      [ myNode
+      , myNode
+      ]
+      []
+    ]
+
+
+a :: Int -> Int
+a x =
+  let a' = 3 :: Int
+  in a' + x
+
+
+listen :: Network MyData -> S.Signal MyData
+listen nw = S.constant Bang
+
+
+viewNetwork :: forall e. Network MyData -> H.Markup e
+viewNetwork nw =
+  H.p $ H.text (fold ["A", "B"] <> show (a 5))
+
+viewData :: forall e. MyData -> H.Markup e
+viewData d =
+  H.p $ H.text (fold ["A", "B"] <> show (a 5))
+
+
+view :: ∀ e. String -> H.Markup e
+view s =
   H.div
-    $ H.text "Hello, PureScript!"
+    $ H.text s
 
 
 -- main function with a custom patch
@@ -43,8 +107,20 @@ view =
     --     (\s -> map show s S.~> C.log)
 
 
-main :: ∀ e. Eff (dom :: DOM | e) Unit
+main :: ∀ e. Eff (dom :: DOM, console :: C.CONSOLE | e) Unit
 main = do
   documentType <- document =<< window
   element <- getElementById (ElementId "app") $ htmlDocumentToNonElementParentNode documentType
-  for_ element (render <@> view)
+  --for_ element (render <@> viewData Bang)
+  for_ element (\element ->
+    S.runSignal (map (\t -> render element $ view t) (S.constant "abc"))
+  )
+  -- for_ element (S.runSignal (map (render ) S.constant myNetwork))
+
+
+  -- nwSignal <- S.constant myNetwork
+  --game <- S.foldp step start signal
+  -- H.render $ render myNetwork
+  -- S.runSignal (map render $ S.constant myNetwork)
+  -- dataSignal <- S.constant Bang
+  -- S.runSignal (map renderData dataSignal)
