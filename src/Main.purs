1aadbd1afc2a6530d8fdcbb9ee23c85d06a00557
Ulric Wilfred <shaman.sir@gmail.com>
2018-03-14 01:14:52 +0100

    got the event cycle working on a simple example

--- src/Main.purs
+++ src/Main.purs
@@ -4,22 +4,27 @@ import Data.Tuple
 import Data.Tuple.Nested ((/\))
 import Prelude
 
+import Data.Foldable (for_, fold)
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console as C
-import Control.Monad.Free (Free)
+
 import DOM (DOM)
 import DOM.HTML (window)
 import DOM.HTML.Types (htmlDocumentToNonElementParentNode)
 import DOM.HTML.Window (document)
 import DOM.Node.NonElementParentNode (getElementById)
 import DOM.Node.Types (ElementId(..), Element)
-import Data.Foldable (for_, fold)
-import Rpd as Rpd
+import DOM.Event.Event (Event)
+import DOM.Event.EventTarget (EventListener, eventListener)
+
 import Signal as S
 import Signal.Channel as SC
 import Signal.Time as ST
+
 import Text.Smolder.HTML as H
 import Text.Smolder.Markup as H
+import Text.Smolder.Markup ((#!), on)
 import Text.Smolder.Renderer.DOM (render)
 
 import Rpd as R
@@ -58,29 +63,62 @@ a x =
   in a' + x
 
 
-main :: ∀ e. Eff (dom :: DOM, console :: C.CONSOLE, channel :: SC.CHANNEL | e) Unit
+data MyEvent = EventOne | EventTwo
+
+type Listener e = MyEvent -> Eff ( console :: C.CONSOLE | e ) Unit
+
+type Model = Int
+
+update :: forall e. MyEvent -> Model -> H.Markup e
+update evt model =
+     H.div $ H.text "Test"
+
+render' :: forall e. Model -> H.Markup MyEvent
+render' model =
+    H.div
+      #! on "click" EventOne
+      $ H.text "Foo"
+
+listener :: forall e. Event -> Eff ( console :: C.CONSOLE | e ) Unit
+listener evt =
+  C.log "got the event"
+
+listenerHtml :: forall e. Element -> Event -> Eff ( dom :: DOM | e ) Unit
+listenerHtml trg evt =
+  render trg (H.div $ H.text "Got the event")
+
+main :: ∀ e. Eff (dom :: DOM, console :: C.CONSOLE | e) Unit
 main = do
   documentType <- document =<< window
   element <- getElementById (ElementId "app") $ htmlDocumentToNonElementParentNode documentType
-  channel <- SC.channel Bang
-  let signal = SC.subscribe channel
-  --for_ element (render <@> viewData Bang)
   for_ element (\element -> do
-    runRpd element signal viewData myNetwork
+    render element (H.div #! on "click" (eventListener listener) $ H.text "Foo")
+    render element (H.div #! on "click" (eventListener $ listenerHtml element) $ H.text "Bar")
   )
-  SC.send channel $ Str' "test"
-  -- let every300s = (ST.every 300.0) S.~> (\_ -> SC.send channel (Int' 300))
-  -- S.runSignal every300s
+
+-- main :: ∀ e. Eff (dom :: DOM, console :: C.CONSOLE, channel :: SC.CHANNEL | e) Unit
+-- main = do
+--   documentType <- document =<< window
+--   element <- getElementById (ElementId "app") $ htmlDocumentToNonElementParentNode documentType
+--   channel <- SC.channel Bang
+--   let signal = SC.subscribe channel
+--   --for_ element (render <@> viewData Bang)
+--   for_ element (\element -> do
+--     runRpd element signal viewData myNetwork
+--   )
+--   SC.send channel $ Str' "test"
+--   -- let every300s = (ST.every 300.0) S.~> (\_ -> SC.send channel (Int' 300))
+--   -- S.runSignal every300s
 
 
-runRpd targetElm dataSignal renderData network = do
-  S.runSignal (map (\t -> render targetElm $ renderData t) dataSignal)
-  render targetElm $ Render.network network
+-- runRpd targetElm dataSignal renderData network = do
+--   S.runSignal (map (\t -> render targetElm $ renderData t) dataSignal)
+--   render targetElm $ Render.network network
 
 
-viewData :: ∀ e. MyData -> H.Markup e
-viewData d =
-  H.p $ H.text $ case d of
-    Bang -> "Bang"
-    Str' str -> "String: " <> str
-    Int' int -> "Int: " <> show int
+-- viewData :: forall e. MyData -> H.Markup (Render.Listener e)
+-- viewData d =
+--   H.p $ H.text $ case d of
+--     Bang -> "Bang"
+--     Str' str -> "String: " <> str
+--     Int' int -> "Int: " <> show int
--- src/Render.purs
+++ src/Render.purs
@@ -1,19 +1,33 @@
 module Render
     ( network
+    , Event
+    , Listener
     ) where
 
 import Prelude
 
-import Text.Smolder.HTML as H
-import Text.Smolder.Markup as H
--- import Text.Smolder.Renderer.DOM (render)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Foldable (for_)
 import Data.Array (length)
+
+import Text.Smolder.HTML as H
+import Text.Smolder.Markup as H
+import Text.Smolder.Markup ((#!), on)
+-- import Text.Smolder.Renderer.DOM (render)
 
 import Rpd as R
 
 
-network :: forall e d. R.Network d -> H.Markup e
+data Event
+    = Connect String String
+    | Drag Int Int
+
+
+type Listener e = (Event -> Eff ( console :: CONSOLE | e ) Unit)
+
+
+network :: forall e d. R.Network d -> H.Markup (Listener e)
 network (R.Network patches) =
     H.div $ do
         H.p $ H.text "Network"
@@ -21,7 +35,7 @@ network (R.Network patches) =
         for_ patches patch
 
 
-patch :: forall e d. R.Patch d -> H.Markup e
+patch :: forall e d. R.Patch d -> H.Markup (Listener e)
 patch (R.Patch label nodes links) =
     H.div $ do
         H.p $ H.text $ "Patch: " <> label
@@ -30,7 +44,7 @@ patch (R.Patch label nodes links) =
         for_ nodes node
 
 
-node :: forall e d. R.Node d -> H.Markup e
+node :: forall e d. R.Node d -> H.Markup (Listener e)
 node (R.Node name inlets outlets _) =
     H.div $ do
         H.p $ H.text $ "Node: " <> name
@@ -40,13 +54,15 @@ node (R.Node name inlets outlets _) =
         for_ outlets outlet
 
 
-inlet :: forall e d. R.Inlet d -> H.Markup e
+inlet :: forall e d. R.Inlet d -> H.Markup (Listener e)
 inlet (R.Inlet label _) =
     H.div $ do
-        H.p $ H.text $ "Inlet: " <> label
+        H.p #! on "click" (\_ -> log "a") $ H.text $ "Inlet: " <> label
+    where
+        evt = Connect "foo" "bar"
 
 
-outlet :: forall e d. R.Outlet d -> H.Markup e
+outlet :: forall e d. R.Outlet d -> H.Markup (Listener e)
 outlet (R.Outlet label _) =
     H.div $ do
         H.p $ H.text $ "Outlet: " <> label
