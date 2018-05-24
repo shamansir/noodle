5c395d582e1cf0524cfd9643b6ad172e2506da65
Ulric Wilfred <shaman.sir@gmail.com>
2018-05-24 23:34:16 +0200

    trying to write tests for Rpd.run

--- test/Main.purs
+++ test/Main.purs
@@ -1,28 +1,39 @@
 module Test.Main where
 
 import Prelude
+
 import Data.Time.Duration (Milliseconds(..))
+
 import Control.Monad.Aff (delay)
+import Control.Monad.Aff (Aff, attempt, delay, makeAff, runAff, launchAff, throwError, try)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+
 import Test.Spec (pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
 
-main :: Eff (RunnerEffects ()) Unit
+import FRP (FRP)
+
+import Rpd (run) as Rpd
+
+import RPDTest.Network.TestCreate (network) as TestCreate
+
+main :: Eff (RunnerEffects (frp :: FRP)) Unit
 main = run [consoleReporter] do
-  describe "purescript-spec" do
-    describe "Attributes" do
-      it "awesome" do
-        let isAwesome = true
-        isAwesome `shouldEqual` true
-      pending "feature complete"
-    describe "Features" do
-      it "runs in NodeJS" $ pure unit
-      it "runs in the browser" $ pure unit
-      it "supports streaming reporters" $ pure unit
-      it "supports async specs" do
-        delay (Milliseconds 100.0)
-        res <-  pure "Alligator"
-        res `shouldEqual` "Alligator"
-      it "is PureScript 0.10.x compatible" $ pure unit
+  describe "RPD" do
+    describe "creating" do
+      it "constructing the network works" do
+        liftEff $ Rpd.run (\_ -> pure unit) TestCreate.network
+        pure unit
+    describe "connecting nodes" do
+      pure unit
+      -- it "runs in NodeJS" $ pure unit
+      -- it "runs in the browser" $ pure unit
+      -- it "supports streaming reporters" $ pure unit
+      -- it "supports async specs" do
+      --   delay (Milliseconds 100.0)
+      --   res <-  pure "Alligator"
+      --   res `shouldEqual` "Alligator"
+      -- it "is PureScript 0.10.x compatible" $ pure unit
---
+++ test/Network/TestCreate.purs
@@ -0,0 +1,52 @@
+module RPDTest.Network.TestCreate
+    ( network, MyData ) where
+
+import Prelude
+
+import Rpd as R
+import Rpd.Flow (flow) as R
+
+import Data.Map as Map
+import Data.Maybe (fromMaybe)
+
+import FRP.Event as Event
+import FRP.Event.Time (interval)
+
+data MyData
+  = Bang
+  | Str' String String
+  | Num' String Int
+
+node :: String -> R.LazyNode MyData
+node nodeId =
+  R.node "f"
+    [ R.inlet "a" -- WithDefault "a" (Str' (nodeId <> "a") "i")
+    , R.inletWithDefault "b" $ Str' (nodeId <> "b") "test"
+    , R.inlet' "f" $ R.flow $ map (Num' (nodeId <> "f")) $ interval 5000
+    , R.inlet "d" -- (ST.every ST.second S.~> Num' (nodeId <> "d"))
+    , R.inlet "e" -- WithDefault "e" (Num' (nodeId <> "e") 3.0)
+    ]
+    [ R.outlet "c"
+    , R.outlet' "x" $ R.flow
+        $ map (Num' (nodeId <> "x"))
+        $ Event.fold (\_ n -> n + 1) (interval 5000) 0
+    , R.outlet' "y" $ R.flow
+        $ map (Num' (nodeId <> "y"))
+        $ Event.fold (\_ n -> n + 1) (interval 2000) 0
+    ]
+    -- (\_ -> [ "c" /\ Int' 10 ] )
+
+
+network :: R.Network MyData
+network =
+  R.network
+    [ R.patch "Patch One"
+      [ node "1"
+      , R.processWith processF $ node "2"
+      ] -- >>> connect (patch.getNode 0) "a" (patch.getNode 1) "b"
+    ]
+  where
+    processF inputs | Map.isEmpty inputs = Map.empty
+    processF inputs | Map.member "d" inputs =
+      Map.singleton "c" $ fromMaybe Bang $ Map.lookup "d" inputs
+    processF inputs = Map.empty
