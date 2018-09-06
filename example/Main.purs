638981d861ac9edb1171267b9890556ab9f6e357
Ulric Wilfred <shaman.sir@gmail.com>
2018-09-06 10:41:54 +0200

    fix example to compile

--- example/Main.purs
+++ example/Main.purs
@@ -3,20 +3,23 @@ module Example.Main where
 import Prelude
 
 import Effect (Effect)
+import Data.Const (Const)
 
 import Spork.Html (Html)
 import Spork.Html as H
-import Spork.PureApp (PureApp)
-import Spork.PureApp as PureApp
+import Spork.App (App)
+import Spork.App as App
 
 import Rpd (RpdError, Rpd, Network, emptyNetwork)
+import Rpd (init) as Rpd
 import Rpd.Render (update, once, Renderer, proxy') as Render
 import Rpd.Render (Message(..)) as Ui
 import Rpd.Render.Terminal (view) as TerminalRenderer
 import Rpd.Render.Terminal (terminalRenderer)
+import Spork.Interpreter (never)
 
 
-type Model d = Rpd d
+type Model d = Rpd (Network d)
 type Action d = Ui.Message d
 
 
@@ -27,28 +30,43 @@ sporkRenderer =
       (const $ H.div [] [])
       inject
       (H.div [] [])
-
-
-inject :: forall d. (Ui.Message d -> Effect Unit) -> String -> Html (Action d)
-inject pushMsg strView =
-  H.div [] []
-
-
-update = flip Render.update
+  where
+    inject :: (Ui.Message d -> Effect Unit) -> String -> Html (Action d)
+    inject pushMsg strView =
+      H.div [] []
 
 
 render ∷ forall d. Model d → Html (Action d)
 render rpd =
-  let effHtml = Render.once sporkRenderer rpd
+  let (effHtml :: Effect (Html (Action d))) = Render.once sporkRenderer rpd
+  -- TerminalRenderer.view
   in H.div []
     [ H.button
         [ H.onClick (H.always_ Ui.Bang) ]
         [ H.text "Hit me" ]
     ]
 
-app ∷ forall d. PureApp (Model d) (Action d)
-app = { update, render, init: pure emptyNetwork }
+update :: forall d. Model d -> Action d -> App.Transition Effect (Model d) (Action d)
+update model action =
+  { model: model >>= Render.update action
+  , effects: mempty
+  }
 
 
-main ∷ Effect Unit
-main = void $ PureApp.makeWithSelector app "#app"
+init :: forall d. App.Transition Effect (Model d) (Action d)
+init =
+  { model: Rpd.init "a"
+  , effects: mempty
+  }
+
+app ∷ forall d. App Effect (Const Void) (Model d) (Action d)
+app =
+  { render: render
+  , update: update
+  , subs: const mempty
+  , init: init
+  }
+
+
+-- main ∷ Effect Unit
+-- main = void $ App.makeWithSelector app "#app"
