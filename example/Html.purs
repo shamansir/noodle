b63c490e2e8d15f70ce50cbe29259ee9a56d2c5b
Ulric Wilfred <shaman.sir@gmail.com>
2019-01-09 05:25:23 +0100

    preparing the Html example

--- example/Html.purs
+++ example/Html.purs
@@ -1,3 +1,14 @@
 module Example.Html where
 
-foo = 42
+import Prelude
+
+import Effect (Effect)
+
+import Rpd.Network (empty) as Network
+import Rpd.Renderer.Html.Html (htmlRenderer)
+import Rpd.Renderer.Html.VDom as VDom
+
+main :: Effect Unit
+main =
+    VDom.embed "#app" ?wh htmlRenderer
+        $ Network.empty "foo"
--- src/Rpd/Renderer/Html/Html.purs
+++ src/Rpd/Renderer/Html/Html.purs
@@ -1,3 +1,57 @@
 module Rpd.Renderer.Html.Html where
 
-foo = 42
+import Data.Either (Either)
+import Data.Tuple.Nested (type (/\), (/\))
+
+import Rpd.API (RpdError) as R
+import Rpd.Network (Network) as R
+import Rpd.RenderMUV (Renderer(..), PushMsg, Message) as R
+
+import Spork.Html (Html)
+import Spork.Html as H
+
+
+type Model =
+    {
+    }
+
+
+data Msg
+    = ClickAt (Int /\ Int)
+
+
+type Message d = R.Message d Msg
+
+
+type PushMsg d = R.PushMsg d Msg
+
+
+type View = Html Msg
+
+
+init :: Model
+init =
+    {
+    }
+
+
+type HtmlRenderer d = R.Renderer d Model View Msg
+
+
+htmlRenderer :: forall d. HtmlRenderer d
+htmlRenderer =
+    R.Renderer
+        { from : H.div [] []
+        , init : init
+        , update
+        , view
+        }
+
+
+view :: forall d. PushMsg d -> Either R.RpdError (Model /\ R.Network d) -> View
+view _ _ = H.div [] []
+
+
+update :: forall d. Message d -> (Model /\ R.Network d) -> (Model /\ Array (Message d))
+update _ (ui /\ _) =
+    ui /\ []
