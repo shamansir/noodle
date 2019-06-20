55ebd7bfee17f9dc9f4e3fecba7bd4218eb56773
Ulric Wilfred <shaman.sir@gmail.com>
2019-06-20 17:52:48 +0200

    prototype the MUV system with collecting effects and performing them after every update

--- tmp/TestMUV.purs
+++ tmp/TestMUV.purs
@@ -5,7 +5,11 @@ import Prelude
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
+
 import Data.Either (Either, either)
+import Data.Tuple.Nested ((/\), type (/\))
+import Data.Traversable (traverse_)
+
 import Control.Monad.Except.Trans (ExceptT, runExceptT)
 
 import FRP.Event (Event)
@@ -13,58 +17,90 @@ import FRP.Event as Event
 
 import Debug.Trace as DT
 
+import UUID (UUID)
 import UUID as UUID
 
 data Error = Error String
 
-type Program a = ExceptT Error Effect a
+type Program a = Either Error a
 
 
-data Msg = MsgOne | MsgTwo
+data Msg
+    = MsgOne
+    | MsgTwo
+    | Start
+    | MakeUUID (UUID -> Msg)
+    | Store UUID
+
 
 
 instance showMsg :: Show Msg where
     show MsgOne = "MSG-ONE"
     show MsgTwo = "MSG-TWO"
+    show Start = "START"
+    show (Store uuid) = "STORE-UUID: " <> show uuid
+    show (MakeUUID _) = "MAKE-UUID"
+
+
+type EffectsToPerform = Array (Effect Msg)
 
 
 runMUV
     :: forall model view
-     . Event Msg
+     . { event :: Event Msg, push :: Msg -> Effect Unit }
     -> Program model
-    -> (Msg -> Program model -> Program model)
-    -> (Either Error model -> view)
-    -> Event (Effect view)
-runMUV messages init userUpdate userView =
+    -> (Msg -> Program model -> Program model /\ EffectsToPerform)
+    -> (Program model -> view)
+    -> Effect (Event view)
+runMUV { event : messages, push : pushMessage } init userUpdate userView = do
     let
-        updates = Event.fold update messages init
-        views = view <$> updates
-    in views
+        updates =
+            Event.fold
+                (\msg (model /\ _) -> userUpdate msg model)
+                messages
+                (init /\ [])
+    { event : views, push : pushView } <- Event.create
+    _ <- Event.subscribe updates (performEffects pushView pushMessage)
+    --     views = view <$> updates
+    -- in views
+    pure views
     where
-        update :: Msg -> Program model -> Program model
-        update msg model =
-            let _ = DT.spy "msg" msg
-            in userUpdate msg model
-        view :: Program model -> Effect view
+        update
+            :: Msg
+            -> Program model /\ EffectsToPerform
+            -> Program model /\ EffectsToPerform
+        update msg (model /\ _) =
+            -- let _ = DT.spy "msg" msg
+            userUpdate msg model
+        view :: Program model -> view
         view program =
-            userView <$> runExceptT program
+            userView program
+        performEffects
+            :: (view -> Effect Unit)
+            -> (Msg -> Effect Unit)
+            -> Program model /\ EffectsToPerform
+            -> Effect Unit
+        performEffects pushView pushMsg (model /\ effects) =
+            traverse_ identity effects
 
 
 main :: Effect Unit
 main = do
-    { event : messages, push } <- Event.create
-    let views = runMUV messages (pure "|") update view
-    _ <- Event.subscribe views \effV -> effV >>= log
-    push MsgOne
-    push MsgTwo
-    push MsgTwo
+    { event : messages, push : pushMessage } <- Event.create
+    views <- runMUV { event : messages, push : pushMessage } (pure "|") update view
+    _ <- Event.subscribe views log
+    pushMessage MsgOne
+    pushMessage MsgTwo
+    pushMessage MsgTwo
     pure unit
     where
-        update msg model =
-            model >>= \prev -> do
-                uuid <- liftEffect UUID.new
-                let _ = DT.spy "uuid" uuid
-                pure $ "(" <> show msg <> ":" <> UUID.toString uuid <> ")-" <> prev
+        update msg model = model /\ []
+        -- update msg m = m
+        --     case model of
+        --     model >>= \prev -> do
+        --         uuid <- liftEffect UUID.new
+        --         -- let _ = DT.spy "uuid" uuid
+        --         pure $ "(" <> show msg <> ":" <> UUID.toString uuid <> ")-" <> prev
         view errOrModel =
             either (const "ERR") identity errOrModel
 
