96ed1d7504dae20519b4a5ba29b394725fc678fb
Ulric Wilfred <shaman.sir@gmail.com>
2019-08-21 14:32:54 +0200

    add the basic NodeList renderer

---
+++ src/Rpd/Renderer/Html/NodeList.purs
@@ -0,0 +1,23 @@
+module Rpd.Renderer.Html.NodeList where
+
+import Prelude (($), (<$>), class Show, show)
+
+import Rpd.Renderer.Html (View)
+import Rpd.Renderer.Html (core) as R
+import Rpd.API.Action (Action(..), RequestAction(..)) as A
+import Rpd.Path (ToPatch) as P
+
+import Spork.Html as H
+
+render :: forall d c n. Show n => P.ToPatch -> Array n -> View d c n
+render patchPath nodes =
+    H.div []
+        $ createNodeButton <$> nodes
+        where
+            createNodeButton n =
+                H.div
+                    [ H.classes [ "rpd-node-list" ]
+                    , H.onClick $ H.always_ $ R.core
+                        $ A.Request $ A.ToAddNode patchPath (show n) n
+                    ]
+                    [ H.text $ show n ] -- TODO: add toolkit name
