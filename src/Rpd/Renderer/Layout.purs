6f4148731edb9d5b6cd0df3950c30ca282ab24b2
Ulric Wilfred <shaman.sir@gmail.com>
2019-09-22 13:04:04 +0200

    remove the node from layout when user started to drag it

--- src/Rpd/Renderer/Layout.purs
+++ src/Rpd/Renderer/Layout.purs
@@ -33,7 +33,7 @@ type PatchLayout =
     }
 
 
-type NodesLayer = R2.Bin2 Number UUID.ToNode
+type NodesLayer = R2.Bin2 Number UUID.ToNode -- Store the nodes themselves
 type NodesStack = Seq NodesLayer -- Shelving, may be?
 type PinnedNodes = UUID.ToNode /-> Position
 
@@ -49,6 +49,8 @@ layoutOf :: UUID.ToPatch -> Layout -> Maybe PatchLayout
 layoutOf = Map.lookup
 
 
+-- execute the function with the Stacks of the PatchLayout,
+-- may be used to render the Stacks, where `x` is the `view`, then
 withStackOf
     :: forall x
      . (UUID.ToNode -> Position -> Rect -> x)
@@ -62,6 +64,8 @@ withStackOf fn patchLayout =
             fn uuid { x, y } { width, height }
 
 
+-- execute the function with the pinned nodes of the PatchLayout,
+-- may be used to render the pinned nodes, where `x` is the `view`, then
 withPinnedOf
     :: forall x
      . (UUID.ToNode -> Position -> x)
@@ -79,6 +83,7 @@ newLayer (LayerSize { width, height }) =
     R2.container width height
 
 
+-- load the existing nodes from the network into Stacks
 loadIntoStacks
     :: forall d c n
      . LayerSize
@@ -211,3 +216,21 @@ initWithStacks patchToStack =
     patchToStack <#> \stack ->
         let patchLayout = newPatchLayout unit
         in patchLayout { stack = stack } -- TODO: _.stack
+
+
+freeUp :: forall d c n. R.Patch d c n -> R.Node d n -> Layout -> Layout
+freeUp (R.Patch patchUuid _ _) (R.Node nodeUuid nodePath _ _ _) layout =
+    case layout # layoutOf patchUuid of
+        Just patchLayout ->
+            Map.insert patchUuid (removeFromPatchLayout patchLayout) layout
+        Nothing -> layout
+    where
+        removeFromPatchLayout patchLayout =
+            patchLayout
+                { stack =
+                    R2.free nodeUuid <$> patchLayout.stack
+                , pinned =
+                    Map.delete nodeUuid patchLayout.pinned
+                }
+
+
