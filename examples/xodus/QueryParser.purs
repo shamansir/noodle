7809582d79e6c2d550bf5d19a5e475211e3d98bb
Ulric Wilfred <shaman.sir@gmail.com>
2020-05-12 09:02:27 +0200

    Xodus: implement Query parser

--- examples/xodus/QueryParser.purs
+++ examples/xodus/QueryParser.purs
@@ -6,10 +6,13 @@ import Control.Alt ((<|>))
 
 import Data.Array (fromFoldable)
 import Data.Char.Unicode (isSpace, isDigit, isAlphaNum)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.List.NonEmpty as NEL
 import Data.List.NonEmpty (NonEmptyList)
 import Data.String as String
-import Data.Int (fromString)
+import Data.Int (fromString) as Int
+import Data.Number (fromString) as Number
+import Data.Ordering (Ordering(..)) as O
 
 import Text.Parsing.StringParser (Parser, ParseError, runParser, fail)
 import Text.Parsing.StringParser.CodePoints (satisfy, string)
@@ -28,21 +31,34 @@ data SortDirection
     | Descending
 
 
-isTokenChar :: Char -> Boolean
-isTokenChar c | isAlphaNum c = true
-isTokenChar c | c == '_'     = true
-isTokenChar c | c == '-'     = true
-isTokenChar c | otherwise    = false
+data ComparisonOp
+    = EQ
+    | LT
+    | GT
+    | GTE
+    | LTE
 
 
+cOperator :: Parser ComparisonOp
+cOperator =
+        string "is" $> EQ
+    <|> string "==" $> EQ
+    <|> string "<"  $> LT
+    <|> string ">"  $> GT
+    <|> string ">=" $> GTE
+    <|> string "<=" $> LTE
+
+
+{-
 cOperator :: forall a. Eq a => Ord a => Parser (a -> a -> Boolean)
 cOperator =
         string "is" $> (==)
     <|> string "==" $> (==)
-    <|> string "<" $> (<)
-    <|> string ">" $> (>)
+    <|> string "<"  $> (<)
+    <|> string ">"  $> (>)
     <|> string ">=" $> (>=)
     <|> string "<=" $> (<=)
+-}
 
 
 loadString :: NonEmptyList Char -> String
@@ -71,13 +87,74 @@ condition :: Parser Condition
 condition = do
     fieldName <- nextToken
     delim
-    op <- (cOperator :: Parser (String -> String -> Boolean))
+    cOp <- cOperator
     delim
-    value <- nextToken
-    pure $ Condition \_ -> true
+    reqValue <- nextToken
+    pure $ Condition
+        \entity ->
+            case dataOfProperty entity fieldName of
+                Just { name, type : type_, value }
+                    -> compare type_ cOp value reqValue
+                Nothing
+                    -> false
+    where
+        compare "string" cOp value reqValue =
+            (toOp cOp) value reqValue
+        compare "int" cOp value reqValue =
+            toOp cOp <$> Int.fromString value <*> Int.fromString reqValue # fromMaybe false
+        compare "float" cOp value reqValue =
+            toOp cOp <$> Number.fromString value <*> Number.fromString reqValue # fromMaybe false
+        compare _ _ _ _ = false
 
 
 comparison :: Parser Comparison
 comparison = do
     fieldName <- nextToken
-    pure $ Comparison \_ _ -> EQ
+    delim
+    dir <- direction
+    pure $ Comparison
+        \entityA entityB ->
+            fromMaybe O.EQ $ do
+                { type : typeA, value : valueA } <- dataOfProperty entityA fieldName
+                { type : typeB, value : valueB } <- dataOfProperty entityA fieldName
+                pure $ compare typeA typeB dir valueA valueB
+    where
+        compare "string" "string" dir valueA valueB =
+            (dirToOp dir) valueA valueB
+        compare "int" "int" dir valueA valueB =
+            dirToOp dir
+                <$> Int.fromString valueA
+                <*> Int.fromString valueB
+                # fromMaybe O.EQ
+        compare "float" "float" dir valueA valueB =
+            dirToOp dir
+                <$> Number.fromString valueA
+                <*> Number.fromString valueB
+                # fromMaybe O.EQ
+        compare _ _ _ _ _ = O.EQ
+
+
+isTokenChar :: Char -> Boolean
+isTokenChar c | isAlphaNum c = true
+isTokenChar c | c == '_'     = true
+isTokenChar c | c == '-'     = true
+isTokenChar c | otherwise    = false
+
+
+toOp :: forall a. Eq a => Ord a => ComparisonOp -> (a -> a -> Boolean)
+toOp EQ = (==)
+toOp LT = (<)
+toOp GT = (>)
+toOp LTE = (<=)
+toOp GTE = (>=)
+
+
+dirToOp :: forall a. Eq a => Ord a => SortDirection -> (a -> a -> O.Ordering)
+dirToOp Ascending a b | a == b = O.EQ
+dirToOp Ascending a b | a < b = O.LT
+dirToOp Ascending a b | a > b = O.GT
+dirToOp Ascending a b | otherwise = O.EQ
+dirToOp Descending a b | a == b = O.EQ
+dirToOp Descending a b | a < b = O.GT
+dirToOp Descending a b | a > b = O.LT
+dirToOp Descending a b | otherwise = O.EQ
