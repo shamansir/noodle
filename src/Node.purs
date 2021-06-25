6ef7367669743f0e566a4aec5a68c1d44917785e
Ulric Wilfred <shaman.sir@gmail.com>
2021-06-25 15:04:57 +0200

    working on the function to send values to outlets channel

--- src/Node.purs
+++ src/Node.purs
@@ -1,6 +1,6 @@
 module Node where
 
-import Prelude (bind, pure, ($), (#), flip, (<$>))
+import Prelude (bind, pure, ($), (#), flip, (<$>), (>>>), (<<<), (>>=), (=<<), unit, Unit)
 
 import Control.Applicative (class Applicative, class Apply)
 
@@ -11,12 +11,15 @@ import Data.Tuple (uncurry)
 import Data.Tuple.Nested ((/\), type (/\))
 import Data.Map (Map)
 import Data.Map as Map
+import Data.Newtype (unwrap, class Newtype)
+import Data.Traversable (traverse, sequence)
 
 import Effect (Effect)
 import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
 
 import Signal (Signal)
-import Signal (foldp) as Signal
+import Signal (foldp, unwrap, flatten, constant) as Signal
 import Signal.Channel (Channel)
 import Signal.Channel as Ch
 
@@ -29,32 +32,28 @@ import Signal.Channel as Ch
 
 -- type Node''' d = Node' Identity Unit
 
-data Node m d =
+data Node d =
     Node
-        (Signal (String /\ d) /\ Signal (m (String /\ d)))
+        (Signal (String /\ d) /\ Signal (Effect (String /\ d)))
 
 
 --type Node = NodeM Identity
 
-type EffNode = Node Effect
 
-type AffNode = Node Aff
-
-
-type NodeDef m d = { node :: Node m d, in :: Channel (String /\ d), out :: Channel (m (String /\ d)) }
+type NodeDef d = { node :: Node d, in :: Channel (String /\ d), out :: Channel (Effect (String /\ d)) }
 
 
--- data Receive d = Receive (Map String d)
+newtype Receive d = Receive (Map String d)
 
--- data Send d = Send (Map String d)
+
+newtype Send d = Send (Map String d)
 
 
 fromFn
-    :: forall m d
-     . Applicative m
-    => d
-    -> ((String -> Maybe d) -> m (String -> Maybe d))
-    -> Effect (NodeDef m d)
+    :: forall d
+     . d
+    -> (Receive d -> Effect (Send d))
+    -> Effect (NodeDef d)
 fromFn def fn = do
     inlets_chan <- Ch.channel ("bang" /\ def)
     outlets_chan <- Ch.channel (pure $ "bang" /\ def)
@@ -62,10 +61,27 @@ fromFn def fn = do
         inlets = Ch.subscribe inlets_chan
         outlets = Ch.subscribe outlets_chan
         node = Node (inlets /\ outlets)
-        maps :: Signal (Map String d)
+        --maps :: Signal (Map String d)
         maps = inlets # Signal.foldp (uncurry Map.insert) Map.empty
-        fn_signal :: Signal (m (String -> Maybe d))
-        fn_signal = (\values -> fn (flip Map.lookup $ values)) <$> maps
+        --fn_signal :: Signal (Effect (Send d))
+        fn_signal = (Receive >>> fn) <$> maps
+        sendFx :: Signal (Effect (Array (Effect Unit)))
+        sendFx =
+            ((flip (>>=))
+                \(Send map) ->
+                    pure $ (Ch.send outlets_chan <<< pure) <$> Map.toUnfoldable map
+            ) <$> fn_signal
+        out_signal :: Effect (Signal Unit)
+        out_signal =
+            liftEffect
+                =<< Signal.unwrap
+                <$> (\sig -> Signal.flatten sig $ pure unit)
+                <$> Signal.unwrap sendFx
+    _ <- out_signal
     pure { node, in : inlets_chan, out : outlets_chan }
 
 -- fromFn' :: (d -> d) -> Node''' d
+
+
+receive :: forall d. String -> Receive d -> Maybe d
+receive label (Receive r) = Map.lookup label r -- unwrap >>> flip Map.lookup
