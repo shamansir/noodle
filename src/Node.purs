4a09005393dd22fa92fdb6941dccf51d0c9916fc
Ulric Wilfred <shaman.sir@gmail.com>
2021-06-25 15:55:22 +0200

    simplify code for sending values through the outlets channel

--- src/Node.purs
+++ src/Node.purs
@@ -12,14 +12,14 @@ import Data.Tuple.Nested ((/\), type (/\))
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Newtype (unwrap, class Newtype)
-import Data.Traversable (traverse, sequence)
+import Data.Traversable (traverse, traverse_, sequence)
 
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 
 import Signal (Signal)
-import Signal (foldp, unwrap, flatten, constant) as Signal
+import Signal (foldp, unwrap, flatten, constant, runSignal) as Signal
 import Signal.Channel (Channel)
 import Signal.Channel as Ch
 
@@ -34,13 +34,13 @@ import Signal.Channel as Ch
 
 data Node d =
     Node
-        (Signal (String /\ d) /\ Signal (Effect (String /\ d)))
+        (Signal (String /\ d) /\ Signal (String /\ d))
 
 
 --type Node = NodeM Identity
 
 
-type NodeDef d = { node :: Node d, in :: Channel (String /\ d), out :: Channel (Effect (String /\ d)) }
+type NodeDef d = { node :: Node d, in :: Channel (String /\ d), out :: Channel (String /\ d) }
 
 
 newtype Receive d = Receive (Map String d)
@@ -56,31 +56,25 @@ fromFn
     -> Effect (NodeDef d)
 fromFn def fn = do
     inlets_chan <- Ch.channel ("bang" /\ def)
-    outlets_chan <- Ch.channel (pure $ "bang" /\ def)
+    outlets_chan <- Ch.channel ("bang" /\ def)
     let
         inlets = Ch.subscribe inlets_chan
         outlets = Ch.subscribe outlets_chan
         node = Node (inlets /\ outlets)
-        --maps :: Signal (Map String d)
+        maps :: Signal (Map String d)
         maps = inlets # Signal.foldp (uncurry Map.insert) Map.empty
-        --fn_signal :: Signal (Effect (Send d))
+        fn_signal :: Signal (Effect (Send d))
         fn_signal = (Receive >>> fn) <$> maps
-        sendFx :: Signal (Effect (Array (Effect Unit)))
-        sendFx =
-            ((flip (>>=))
-                \(Send map) ->
-                    pure $ (Ch.send outlets_chan <<< pure) <$> Map.toUnfoldable map
-            ) <$> fn_signal
-        out_signal :: Effect (Signal Unit)
-        out_signal =
-            liftEffect
-                =<< Signal.unwrap
-                <$> (\sig -> Signal.flatten sig $ pure unit)
-                <$> Signal.unwrap sendFx
-    _ <- out_signal
+        sendFx :: Signal (Effect Unit)
+        sendFx = ((=<<) $ sendAllTo outlets_chan) <$> fn_signal
+    _ <- Signal.runSignal sendFx
     pure { node, in : inlets_chan, out : outlets_chan }
 
 -- fromFn' :: (d -> d) -> Node''' d
+
+sendAllTo :: forall d. Channel (String /\ d) -> Send d -> Effect Unit
+sendAllTo sendTo (Send map) =
+    traverse_ (Ch.send sendTo) $ (Map.toUnfoldable map :: Array (String /\ d))
 
 
 receive :: forall d. String -> Receive d -> Maybe d
