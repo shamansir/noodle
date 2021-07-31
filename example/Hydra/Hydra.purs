1e20b5d3b4fe821df93f8670948d868ae144b044
Ulric Wilfred <shaman.sir@gmail.com>
2021-07-31 10:50:21 +0200

    add more Hydra functions and define the Hydra type better

--- example/Hydra/Hydra.purs
+++ example/Hydra/Hydra.purs
@@ -5,6 +5,13 @@ import Prelude (($), (<<<))
 import Data.Maybe (Maybe(..))
 
 
+type Fn = { name :: String, args :: Array String, out :: String }
+
+
+class ToFn a where
+    toFn :: a -> Fn
+
+
 data Value
     = Num Number
     | Mouse
@@ -13,12 +20,71 @@ data Value
     -- Harmonic Int
     -- FN (Time -> Value)
 
+data Source
+    = Noise { scale :: Value, offset :: Value }
+    | Voronoi { scale :: Value, speed :: Value, blending :: Value }
+    | Osc { freq :: Value, sync :: Value, offset :: Value }
+    | Shape { sides :: Value, radius :: Value, smoothing :: Value }
+    | Gradient { speed :: Value }
+    | Solid { r :: Value, g :: Value, b :: Value, a :: Value }
+
+
+data Geometry
+    = Rotate { angle :: Value, speed :: Value }
+    | Scale { amount :: Value, xMult :: Value, yMult :: Value, offsetX :: Value, offsetY :: Value }
+    | Pixelate { pixelX :: Value, pixelY :: Value }
+    | Repeat { repeatX :: Value, repeatY :: Value, offsetX :: Value, offsetY :: Value }
+    | RepeatX { reps :: Value, offset :: Value }
+    | RepeatY { reps :: Value, offset :: Value }
+    | Kaleid { nSides :: Value }
+    | ScrollX { scrollX :: Value, speed :: Value }
+    | ScrollY { scrollY :: Value, speed :: Value }
+
+
+data Color
+    = Posterize { bins :: Value, gamma :: Value }
+    | Shift { r :: Value, g :: Value, b :: Value, a :: Value }
+    | Invert { amount :: Value }
+    | Contrast { amount :: Value }
+    | Brightness { amount :: Value }
+    | Luma { treshold :: Value, tolerance :: Value }
+    | Tresh { treshold :: Value, tolerance :: Value }
+    | Color { r :: Value, g :: Value, b :: Value, a :: Value }
+    | Saturate { amount :: Value }
+    | Hue { amount :: Value }
+    | Colorama { amount :: Value }
+
+
+data Blend
+    = Add { what :: Source, amount :: Value }
+    | Layer { what :: Source }
+    | Blend { what :: Source, amount :: Value }
+    | Mult { what :: Source, amount :: Value }
+    | Diff { what :: Source }
+    | Mask { what :: Source }
+
+
+data Modulate
+    = ModulateRepeat { what :: Source, repeatX :: Value, repeatY :: Value, offsetX :: Value, offsetY :: Value }
+    | ModulateRepeatX { what :: Source, reps :: Value, offset :: Value }
+    | ModulateRepeatY { what :: Source, reps :: Value, offset :: Value }
+    | ModulateKaleid { what :: Source, nSides :: Value }
+    | ModulateScrollX { what :: Source, scrollX :: Value, speed :: Value }
+    | ModulateScrollY { what :: Source, scrollY :: Value, speed :: Value }
+    | Modulate { what :: Source, amount :: Value }
+    | ModulateScale { what :: Source, multiple :: Value, offset :: Value }
+    | ModulatePixelate { what :: Source, multiple :: Value, offset :: Value }
+    | ModulateRotate { what :: Source, multiple :: Value, offset :: Value }
+    | ModulateHue { what :: Source, amount :: Value }
+
 
 data Hydra
     = None
-    | Osc Value Value Value
-    | Value' Value
-    | Join (Array Hydra)
+    | Hydra Source
+        (Array Geometry)
+        (Array Color)
+        (Array Blend)
+        (Array Modulate)
     | Out (Maybe Int) Hydra
 
 
