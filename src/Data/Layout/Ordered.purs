87fb3ee78ab4bd8a610479dba9ea53f927cab611
Ulric Wilfred <shaman.sir@gmail.com>
2021-08-20 10:33:07 +0200

    implement fitting logic for `Ordered` (a.k.a. `Flex`) layout

--- src/Data/Layout/Ordered.purs
+++ src/Data/Layout/Ordered.purs
@@ -7,14 +7,18 @@ import App.Style.Order (Order)
 
 
 import Data.Maybe (Maybe(..))
-import Data.Vec2 (Size)
+import Data.Vec2 (Size, Pos, (<+>))
+import Data.Vec2 as V2
 import Data.Int (toNumber)
 import Data.Array as Array
-
+import Data.Tuple (fst, snd)
 import Data.Tuple.Nested ((/\), type (/\))
+
+import Data.Bifunctor (class Bifunctor, bimap, lmap)
 
 
 -- TODO: `IsLayout` instance (AutoSizedLayout?)
+-- TODO: Rename to `Flex`?
 
 
 cellSize :: Number
@@ -31,12 +35,37 @@ data Rule
 -- data VBox s a = Vert (Array (s /\ a))
 
 
+data Direction
+    = Horizontal
+    | Vertical
+
+
 data HBox s a = Horz (Array (s /\ a))
 
 data VBox s a = Vert (Array (s /\ HBox s a))
 
 
 type Ordered s a = VBox s a
+
+
+instance functorHBox :: Functor (HBox s) where
+    map :: forall a b. (a -> b) -> HBox s a -> HBox s b
+    map f (Horz items) = Horz $ map f <$> items
+
+
+instance functorVBox :: Functor (VBox s) where
+    map :: forall a b. (a -> b) -> VBox s a -> VBox s b
+    map f (Vert items) = Vert $ map (map f) <$> items
+
+
+instance bifunctorHBox :: Bifunctor HBox where
+    bimap :: forall s1 s2 a b. (s1 -> s2) -> (a -> b) -> HBox s1 a -> HBox s2 b
+    bimap f g (Horz items) = Horz $ bimap f g <$> items
+
+
+instance bifunctorVBox :: Bifunctor VBox where
+    bimap :: forall s1 s2 a b. (s1 -> s2) -> (a -> b) -> VBox s1 a -> VBox s2 b
+    bimap f g (Vert items) = Vert $ bimap f (bimap f g) <$> items
 
 
 auto :: Rule
@@ -60,24 +89,56 @@ make items = Vert $ map Horz <$> items
 
 
 fit :: forall a. Size -> Ordered Rule a -> Ordered Size a
-fit _ _ = Vert [] -- TODO
-
-
-fit' :: Number -> Array Rule -> Array Number
-fit' size rules =
-    fillAutoSize <$> maybeActualSizes
+fit size (Vert vbox) =
+    Vert result
     where
-        isAuto Auto = true
-        isAuto _ = false
-        toKnownSize Auto = Nothing
-        toKnownSize (Fixed n) = Just n
-        toKnownSize (Percentage p) = Just $ size * (toNumber p / 100.0)
-        toKnownSize (Cells c) = Just $ c * cellSize
-        autoCount = Array.length $ Array.filter isAuto $ rules
-        maybeActualSizes =  toKnownSize <$> rules
-        knownSize = Array.foldr (+) 0.0 $ Array.catMaybes maybeActualSizes
-        fillAutoSize (Just n) = n
-        fillAutoSize Nothing = (size - knownSize) / toNumber autoCount
+
+        result :: Array (Size /\ HBox Size a)
+        result =
+            (\(h /\ (Horz hbox)) ->
+                (V2.w size <+> h)
+                /\
+                (Horz $ (lmap (\w -> w <+> h)) <$> fitPlain' (V2.w size) hbox)
+            )
+            <$> verticalFit
+
+        verticalFit :: Array (Number /\ HBox Rule a)
+        verticalFit = fitPlain' (V2.h size) vbox
+
+
+        fitPlain' :: forall x. Number -> Array (Rule /\ x) -> Array (Number /\ x)
+        fitPlain' amount items = Array.zip (fitPlain amount (fst <$> items)) (snd <$> items)
+
+
+        fitPlain :: Number -> Array Rule -> Array Number
+        fitPlain amount rules =
+            fillAutoAmount <$> maybeActualAmounts
+            where
+
+                isAuto Auto = true
+                isAuto _ = false
+
+                toKnownAmount Auto = Nothing
+                toKnownAmount (Fixed n) = Just n
+                toKnownAmount (Percentage p) = Just $ amount * (toNumber p / 100.0)
+                toKnownAmount (Cells c) = Just $ c * cellSize
+
+                autoCount = Array.length $ Array.filter isAuto $ rules
+
+                maybeActualAmounts =  toKnownAmount <$> rules
+
+                knownAmount = Array.foldr (+) 0.0 $ Array.catMaybes maybeActualAmounts
+
+                fillAutoAmount (Just n) = n
+                fillAutoAmount Nothing = (amount - knownAmount) / toNumber autoCount
+
+
+mapSize :: forall s1 s2 a. (s1 -> s2) -> Ordered s1 a -> Ordered s2 a
+mapSize = lmap
+
+
+mapSize' :: forall s1 s2 a. (Direction -> s1 -> s2) -> Ordered s1 a -> Ordered s2 a
+mapSize' f (Vert items) = Vert $ bimap (f Vertical) (lmap $ f Horizontal) <$> items
 
 
 {- tryHorz :: forall a. Rule -> a -> Ordered Rule a -> Maybe (Ordered Rule a)
@@ -86,6 +147,10 @@ tryHorz _ _ ordered = ordered
 
 tryVert :: forall a. Rule -> a -> Ordered Rule a -> Maybe (Ordered Rule a)
 tryVert _ _ ordered = ordered -}
+
+
+find :: forall a. Pos -> Ordered Size a -> Maybe a
+find _ _ = Nothing
 
 
 sizeOf :: forall s a. Eq a => a -> Ordered s a -> Maybe s
@@ -100,8 +165,16 @@ unfold :: forall s a. Ordered s a -> Array (s /\ Array (s /\ a))
 unfold _ = []
 
 
+unfold' :: forall a. Ordered Size a -> Array (Pos /\ Size /\ Array (Pos /\ Size /\ a))
+unfold' _ = []
+
+
 flatten :: forall s a. Ordered s a -> Array (s /\ s /\ a)
 flatten _ = []
+
+
+flatten' :: forall a. Ordered Size a -> Array (Pos /\ Size /\ a)
+flatten' _ = []
 
 
 -- toUnfoldable ::
