769861d733fe0a08a678051221fbcbc7b433c2f7
Ulric Wilfred <shaman.sir@gmail.com>
2021-08-21 11:57:59 +0200

    add some samples to orient onto

---
+++ example/Hydra/Samples/sample.02.hydra.js
@@ -0,0 +1,16 @@
+// https://hydra.ojack.xyz/?sketch_id=malitzin_0
+
+//CNDSD
+//http://malitzincortes.net/
+// sand spirals
+
+osc(3, 0.01, 0.4)
+.color(1.2,1.2,1.3)
+.saturate(0.4)
+.modulateRepeat(osc(2),1, 2, 4, 3)
+.modulateKaleid(osc(12,0.05,0),1)
+.luma (0.4)
+.rotate(4, 0.1,0)
+.modulate(o0, () => mouse.y *0.0002 )
+.scale(1).diff(o1)
+.out(o0)
