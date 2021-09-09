2dd5f2931ae73d78db1857d8e18f88a7730ad6df
Ulric Wilfred <shaman.sir@gmail.com>
2021-09-09 16:45:07 +0200

    one more test for Hydra compilation + remove `H.` in tests

--- test/Hydra/Compilation.purs
+++ test/Hydra/Compilation.purs
@@ -1,6 +1,6 @@
 module Test.Hydra.Compilation where
 
-import Prelude
+import Prelude (($), (#), Unit, discard)
 
 import Control.Monad.Error.Class (class MonadError, class MonadThrow, throwError, try)
 import Effect.Exception (Error, error)
@@ -13,8 +13,7 @@ import Data.Vec2 ((<+>))
 
 
 --import Hydra as H
-import Hydra.API as H
-import Hydra.API ((/*/), (///))
+import Hydra.API
 import Hydra.Compile (compact) as Compiler
 import Hydra.Compile (compile, compileWithRender)
 import Hydra.Queue (Queue)
@@ -32,8 +31,8 @@ spec = do
             Queue.empty `shouldCompileTo` ""
 
         it "just oscillator" $ do
-            (H.osc (H.n 60.0) (H.n 0.1) (H.n 0.0)
-                # H.out
+            (osc (n 60.0) (n 0.1) (n 0.0)
+                # out
             )
                 `shouldCompileTo`
                 "osc(60.0,0.1,0.0).out()"
@@ -42,8 +41,8 @@ spec = do
 
             -- Original: osc(Math.PI*2*10,0).out(o0)
 
-            (H.osc2 (H.pi /*/ H.n 2.0 /*/ H.n 10.0) (H.n 0.0)
-                # H.out' H.o0
+            (osc2 (pi /*/ n 2.0 /*/ n 10.0) (n 0.0)
+                # out' o0
             )
                 `shouldCompileTo`
                 "osc(((Math.PI*2.0)*10.0),0.0,0.0).out(o0)"
@@ -60,18 +59,18 @@ spec = do
 
             -}
 
-            (H.queue
-                [ H.osc2 (H.n 40.0) (H.n 0.0)
-                    # H.out'' H.o0
-                , H.src H.o0
-                    # H.tresh0
-                    # H.out'' H.o1
-                , H.src H.o0
-                    # H.posterize (H.n 3.0) (H.n 1.0)
-                    # H.out'' H.o2
-                , H.src H.o0
-                    # H.pixelate (H.n 20.0) (H.n 20.0)
-                    # H.out'' H.o3
+            (queue
+                [ osc2 (n 40.0) (n 0.0)
+                    # out'' o0
+                , src o0
+                    # tresh0
+                    # out'' o1
+                , src o0
+                    # posterize (n 3.0) (n 1.0)
+                    # out'' o2
+                , src o0
+                    # pixelate (n 20.0) (n 20.0)
+                    # out'' o3
                 ]
             )
                 `shouldCompileWithRenderTo`
@@ -85,9 +84,9 @@ render()"""
 
             -- Original: osc(200, 0).kaleid(99).out(o0)
 
-            (H.osc2 (H.n 200.0) (H.n 0.0)
-                # H.kaleid (H.n 99.0)
-                # H.out' H.o0
+            (osc2 (n 200.0) (n 0.0)
+                # kaleid (n 99.0)
+                # out' o0
             )
                 `shouldCompileTo`
                 "osc(200.0,0.0,0.0).kaleid(99.0).out(o0)"
@@ -103,11 +102,11 @@ render()"""
                 .out(o0)
             -}
 
-            (H.osc2 (H.n 40.0) (H.n 0.0)
-                # H.tresh0
-                # H.kaleid (H.n 99.0)
-                # H.scale3 (H.n 1.0) (H.n 1.0) (H.dyn $ H.windowWidth /// H.windowHeight)
-                # H.out' H.o0
+            (osc2 (n 40.0) (n 0.0)
+                # tresh0
+                # kaleid (n 99.0)
+                # scale3 (n 1.0) (n 1.0) (dyn $ windowWidth /// windowHeight)
+                # out' o0
             )
                 `shouldCompileTo`
                 "osc(40.0,0.0,0.0).tresh(0.5,0.04).kaleid(99.0).scale(1.0,1.0,() => (window.innerWidth/window.innerHeight),0.0,0.0).out(o0)"
@@ -127,17 +126,60 @@ render()"""
                 .out(o0)
             -}
 
-            (H.shape2 (H.n 4.0) (H.n 0.9)
-                # H.diff
-                    (H.src H.o0
-                        # H.scale1 (H.n 0.9)
-                        # H.mask0 (H.shape (H.n 4.0) (H.n 0.9) (H.n 0.01))
-                        # H.rotate1 (H.n 0.1)
+            (shape2 (n 4.0) (n 0.9)
+                # diff
+                    (src o0
+                        # scale1 (n 0.9)
+                        # mask0 (shape (n 4.0) (n 0.9) (n 0.01))
+                        # rotate1 (n 0.1)
                     )
-                # H.out' H.o0
+                # out' o0
             )
                 `shouldCompileTo`
                 "shape(4.0,0.9,0.01).diff(src(o0).scale(0.9,1.0,1.0,0.0,0.0).mask(shape(4.0,0.9,0.01),3.0,0.5).rotate(0.1,0.0)).out(o0)"
+
+        it "reusing pieces (book example #5)" $
+
+            {- Original:
+
+            n = 50;
+            func = () => osc(30,0.1,1).modulate(noise(4,0.1))
+            pix = () => shape(4,0.3).scale(1,1,3).repeat(n,n)
+            pix().mult(func().color(1,0,0).pixelate(n,n)).out(o1)
+            pix().mult(func().color(0,1,0).pixelate(n,n)).scrollX(1/n/3).out(o2)
+            pix().mult(func().color(0,0,1).pixelate(n,n)).scrollX(2/n/3).out(o3)
+
+            solid().add(src(o1),1).add(src(o2),1).add(src(o3),1).out(o0)
+            -}
+            let
+                fifty = n 50.0
+                func = osc (n 30.0) (n 0.1) (n 1.0) # modulate0 (noise (n 4.0) (n 0.1))
+                pix = shape2 (n 4.0) (n 0.3) # scale3 (n 1.0) (n 1.0) (n 3.0) # repeat2 fifty fifty
+            in do
+                (queue
+                    [ pix
+                        # mult0 (func # color3 (n 1.0) (n 0.0) (n 0.0) # pixelate fifty fifty)
+                        # out'' o1
+                    , pix
+                        # mult0 (func # color3 (n 0.0) (n 1.0) (n 0.0) # pixelate fifty fifty)
+                        # scrollX1 (n 1.0 /// fifty /// n 3.0)
+                        # out'' o2
+                    , pix
+                        # mult0 (func # color3 (n 0.0) (n 0.0) (n 1.0) # pixelate fifty fifty)
+                        # scrollX1 (n 2.0 /// fifty /// n 3.0)
+                        # out'' o3
+                    , solid0
+                        # add (src o1) (n 1.0)
+                        # add (src o2) (n 1.0)
+                        # add (src o3) (n 1.0)
+                        # out'' o0
+                    ]
+                )
+                    `shouldCompileTo`
+                    """solid(0.0,0.0,0.0,1.0).add(src(o1),1.0).add(src(o2),1.0).add(src(o3),1.0).out(o0)
+shape(4.0,0.3,0.01).scale(1.0,1.0,3.0,0.0,0.0).repeat(50.0,50.0,0.0,0.0).mult(osc(30.0,0.1,1.0).modulate(noise(4.0,0.1),0.1).color(1.0,0.0,0.0,1.0).pixelate(50.0,50.0),0.5).out(o1)
+shape(4.0,0.3,0.01).scale(1.0,1.0,3.0,0.0,0.0).repeat(50.0,50.0,0.0,0.0).mult(osc(30.0,0.1,1.0).modulate(noise(4.0,0.1),0.1).color(0.0,1.0,0.0,1.0).pixelate(50.0,50.0),0.5).scrollX(((1.0/50.0)/3.0),0.0).out(o2)
+shape(4.0,0.3,0.01).scale(1.0,1.0,3.0,0.0,0.0).repeat(50.0,50.0,0.0,0.0).mult(osc(30.0,0.1,1.0).modulate(noise(4.0,0.1),0.1).color(0.0,0.0,1.0,1.0).pixelate(50.0,50.0),0.5).scrollX(((2.0/50.0)/3.0),0.0).out(o3)"""
 
 
 shouldCompileTo
