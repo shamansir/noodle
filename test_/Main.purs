0eed0b8d1e1f66e1cf1a382d4f6878de13a3bebd
Ulric Wilfred <shaman.sir@gmail.com>
2022-03-07 10:43:23 +0100

    move `App` as rendering and previous tests to a separate backups folders, temoporarily, so that it would be easier to write tests for new functionality

--- test_/Main.purs
+++ test_/Main.purs
@@ -10,9 +10,16 @@ import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 
 import Test.Nodes (spec) as Nodes
+import Test.Layouts (spec) as Layouts
+import Test.Hydra.Compilation (spec) as HydraCompilation
 
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
   describe "Noodle"
     Nodes.spec
+  describe "Layouts"
+    Layouts.spec
+  describe "Hydra" do
+    describe "Compilation"
+      HydraCompilation.spec
---
+++ test_/Test.Signal.js
@@ -0,0 +1,31 @@
+// module Test.Signal
+
+exports.tickP = function tickP(constant, initial, interval, values) {
+    var vals = values.slice();
+    var out = constant(vals.shift());
+    if (vals.length) {
+      setTimeout(function pop() {
+        out.set(vals.shift());
+        if (vals.length) {
+          setTimeout(pop, interval);
+        }
+      }, initial);
+    }
+    return out;
+  };
+
+  exports.incEff = function (val) {
+    return function () { return val + 1; };
+  };
+
+  exports.incAffP = function (right) {
+    return function (val) {
+      return function (callback) {
+        return function () {
+          setTimeout(function () {
+            callback(right(val + 1))();
+          }, 0);
+        };
+      };
+    };
+  };
