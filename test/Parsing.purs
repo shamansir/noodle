ce151c072428b5c275042be398618bd8bed72e94
Ulric Wilfred <shaman.sir@gmail.com>
2023-03-06 16:50:23 +0100

    improved draft for toolkit quick definition parsing

--- test/Parsing.purs
+++ test/Parsing.purs
@@ -2,7 +2,7 @@ module Test.Parsing where
 
 import Prelude
 
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe(..))
 import Data.Either (Either(..))
 import Data.Identity (Identity)
 import Data.String.CodeUnits as String
@@ -46,6 +46,12 @@ newtype FN = FN
   }
 
 
+qfn :: String -> String -> Array { name :: String, type :: String, default :: Maybe String } -> String -> FN
+qfn family name args = qfn' family name $ Just <$> args
+
+qfn' :: String -> String -> Array (Maybe { name :: String, type :: String, default :: Maybe String }) -> String -> FN
+qfn' family name args returns = FN { family, name, args, returns }
+
 
 parses :: forall (s :: Type) (m :: Type -> Type) (a :: Type). MonadThrow Error m => Show a => Eq a => s -> a -> P.ParserT s Identity a -> m Unit
 parses string expected parser =
@@ -56,10 +62,7 @@ parses string expected parser =
       fail $ show error
 
 
-
-
-
-myParser :: forall (m :: Type -> Type). Functor m => Monad m => P.ParserT String m String
+myParser :: forall (m :: Type -> Type). Functor m => Monad m => P.ParserT String m FN
 myParser = do
   family <- validToken
   _ <- Array.some P.space
@@ -68,25 +71,40 @@ myParser = do
   fnName <- validToken
   _ <- Array.some P.space
   _ <- P.string "::"
-  _ <- Array.some P.space
-  values <- P.sepBy validArgument arrowSep
+  _ <- Array.many P.space
+  args <- P.option [] argumentsP
   _ <- Array.many P.space
   _ <- P.string "=>"
   _ <- Array.some P.space
   returnVal <- validToken
-  pure fnName
+  pure $ qfn' family fnName args returnVal
 
   where
-    validToken = String.fromCharArray <$> Array.many P.alphaNum
-    validArgument = validToken
+    validToken = String.fromCharArray <$> Array.some P.alphaNum
+    validArgument = do
+      P.choice
+        [ P.char '?' *> pure Nothing
+        ]
+      -- _ <- validToken
+      -- pure $ Just { name : "", type : "", default : Nothing }
     arrowSep = do
       _ <- Array.many P.space
-      _ <- P.string "->"
-      -- _ <- P.oneOf [ P.string "->", String.singleton <$> P.char '→' ]
+      _ <- P.choice [ P.string "->", String.singleton <$> P.char '→' ]
       _ <- Array.many P.space
       pure unit
+    argumentsP =
+      P.between
+        (P.char '(')
+        (P.char ')')
+        $ Array.fromFoldable <$> P.sepBy validArgument arrowSep
 
 
+-- instance Show FN where
+--   show (FN { family, name, args, returns }) = ""
+
+
+derive newtype instance Show FN
+derive newtype instance Eq FN
 
 
 main :: Effect Unit
@@ -94,9 +112,51 @@ main = launchAff_ $ runSpec [consoleReporter] do
   describe "Toolkit Defs to code" $ do
     it "parsing works" $
         -- file <- readTextFile UTF8 "./hydra.fn.clean.list"
-        -- source : shape :: sides:Value (60) -> radius:Value (0.3) -> smoothing:Value (0.01) -> Texture
+        -- source : shape :: sides:Value (60) -> radius:Value (0.3) -> smoothing:Value (0.01) -> Result
         -- let parseResult = P.runParser "x" $ P.char 'x'
       parses "x" 'x' $ P.char 'x'
 
+    it "parsing with sep works" $ do
+        -- file <- readTextFile UTF8 "./hydra.fn.clean.list"
+        -- source : shape :: sides:Value (60) -> radius:Value (0.3) -> smoothing:Value (0.01) -> Result
+        -- let parseResult = P.runParser "x" $ P.char 'x'
+      let
+        arrowSep = do
+          _ <- Array.many P.space
+          _ <- P.choice [ P.string "->", String.singleton <$> P.char '→' ]
+          _ <- Array.many P.space
+          pure unit
+        qParser = Array.fromFoldable <$> P.char '?' `P.sepBy` arrowSep
+
+      parses "x" [] qParser
+      parses "?" ['?'] qParser
+      parses "? -> ?" [ '?', '?' ] qParser
+      parses "?->?" [ '?', '?' ] qParser
+      parses "?->? ->?" [ '?', '?', '?' ] qParser
+      parses "? -> ? -> ?" [ '?', '?', '?' ] qParser
+
     it "parsing one function with no arguments works" $
-      parses "test : foo :: => Texture" "foo" myParser
+      parses
+        "test : foo :: => Result"
+        (qfn "test" "foo" [] "Result")
+        myParser
+
+    it "parsing one function with an unkwown argument works" $
+      parses
+        "test : foo :: (?) => Result"
+        (qfn' "test" "foo" [ Nothing ] "Result")
+        myParser
+
+    it "parsing one function with two unkwown arguments works" $ do
+      parses
+        "test : foo :: (?->?) => Result"
+        (qfn' "test" "foo" [ Nothing, Nothing ] "Result")
+        myParser
+      parses
+        "test : foo :: (? -> ?) => Result"
+        (qfn' "test" "foo" [ Nothing, Nothing ] "Result")
+        myParser
+      parses
+        "test : foo :: (? ->?) => Result"
+        (qfn' "test" "foo" [ Nothing, Nothing ] "Result")
+        myParser
