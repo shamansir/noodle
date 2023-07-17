175315e32e7f3897d03221cfd30e660ba43d3f3e
Ulric Wilfred <shaman.sir@gmail.com>
2023-07-18 00:31:42 +0200

    the draft for NDF file parser, p.2

--- src/Noodle/Text/NetworkFile/Parser.purs
+++ src/Noodle/Text/NetworkFile/Parser.purs
@@ -12,7 +12,7 @@ import Control.Alt ((<|>))
 import Data.Array (many)
 import Data.String as String
 import Data.String.NonEmpty.CodeUnits as CU
-import Data.String.NonEmpty.Internal as String
+import Data.String.NonEmpty.Internal as StringX
 import Data.Tuple.Nested ((/\), type (/\))
 
 myFile :: String
@@ -27,26 +27,65 @@ number 40 40 num-0
 ~> num-0 0 N 40.0
   """
 
-type Result = String /\ Number
+data Command
+    = MakeNode String Int Int String
+    | Connect String Int String Int
+    | Send String Int String
+    | SendO String Int String
+    | Connect_ String String String String
+    | Send_ String String String
+    | SendO_ String String String
+
+
+derive instance Eq Command
+
+
+newtype Header = Header (String /\ Number)
+
+data Program = Program Header (Array Command)
 
 
 alphaNumTill :: forall a. Parser String a -> Parser String String
 alphaNumTill stopAt =
-  many1Till alphaNum stopAt <#> CU.fromFoldable1 <#> String.toString
+  many1Till alphaNum stopAt <#> CU.fromFoldable1 <#> StringX.toString
 
 
 eol :: Parser String Unit
 eol = char '\n' *> pure unit
 
 
-parser :: Parser String Result
+parser :: Parser String Program
 parser = do
-  str <- alphaNumTill space
-  n <- number
+  toolkit <- alphaNumTill space
+  version <- number
   eol
   _ <- string "osc"
-  pure $ str /\ n
+  pure $ Program (Header $ toolkit /\ version) []
 
+
+class ToCode x where
+  toCode :: x -> String
+
+
+instance ToCode Command where
+    toCode :: Command -> String
+    toCode =
+        case _ of
+            -- Header toolkit version -> toolkit <> " " <> version
+            MakeNode family top left nodeId -> family <> " " <> show top <> " " <> show left <> " " <> nodeId
+            Connect fromNode oindex toNode iindex -> "<> " <> fromNode <> " " <> show oindex <> " " <> toNode <> " " <> show iindex
+            Send nodeId iindex value -> "-> " <> nodeId <> " " <> show iindex <> " " <> value
+            SendO nodeId oindex value -> "~> " <> nodeId <> " " <> show oindex <> " " <> value
+            Connect_ fromNode oname toNode iname -> "<> " <> fromNode <> " " <> oname <> " " <> toNode <> " " <> iname
+            Send_ nodeId iname value -> "-> " <> nodeId <> " " <> iname <> " " <> value
+            SendO_ nodeId oname value -> "~> " <> nodeId <> " " <> oname <> " " <> value
+
+
+instance Show Program where
+  show :: Program -> String
+  show (Program (Header (tk /\ version)) commands) =
+    tk <> " " <> show version <> "\n" <>
+    (String.joinWith "\n" $ toCode <$> commands)
 
 {-
 import Data.Array (fromFoldable, (:))
