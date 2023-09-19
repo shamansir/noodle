e71afcd46c853f744976c686b2962ed559d27821
Ulric Wilfred <shaman.sir@gmail.com>
2023-09-19 23:38:55 +0200

    draft for requesting a list of products, p.2

--- src/CompArts/Product.purs
+++ src/CompArts/Product.purs
@@ -8,13 +8,18 @@ import Effect.Aff (Aff)
 import Data.Either (Either(..))
 import Data.Map as Map
 import Data.Bifunctor (lmap)
+import Data.Lens (preview)
+import Data.Lens.Index (ix)
 
 import Data.Argonaut (decodeJson, jsonParser, JsonDecodeError(..))
 import Data.Argonaut.Core (Json)
+import Data.Argonaut.Prisms (_Array, _Number, _Object)
+import Data.Argonaut.Decode.Class (class DecodeJson)
 
 import Affjax.Node as AJ
 import Affjax.ResponseFormat (json)
 import Foreign.Object as F
+
 
 jsonPath = "https://resources.jetbrains.com/cai/brand-data/products.json"
 
@@ -27,13 +32,30 @@ decodeAsMap str = do
     pure $ Map.fromFoldable $ (F.toUnfoldable obj :: Array _)
 
 
+-- newtype PaletteItem
+--     = PaletteItem { r :: Int, g :: Int, b :: Int, hex :: String }
+
+
+-- derive newtype instance DecodeJson PaletteItem
+
+
 type Product =
     { name :: String
-    , colors :: Array { red :: Int, green :: Int, blue :: Int, hex :: String }
+    , palette :: Array { r :: Int, g :: Int, b :: Int, hex :: String }
     }
 
 
-requestProducts :: Aff (Either AJ.Error (AJ.Response Json))
+type Products =
+    { all :: F.Object Product
+    }
+
+
+requestProducts :: Aff (Either (Either AJ.Error JsonDecodeError) (Map.Map String Product))
 requestProducts =
-    AJ.get json jsonPath
-    -- pure $ Left "foo"
+    AJ.get json jsonPath >>= case _ of
+        Left ajErr -> pure $ Left $ Left ajErr
+        Right jsonResponce ->
+            case (decodeJson jsonResponce.body :: Either JsonDecodeError Products) of
+                Left jsonErr -> pure $ Left $ Right jsonErr
+                Right decoded ->
+                    pure $ Right $ Map.fromFoldable $ (F.toUnfoldable decoded.all :: Array _)
