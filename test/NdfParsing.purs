2e2677419349f409c02aabfc1445f2d43a173fb3
Ulric Wilfred <shaman.sir@gmail.com>
2023-10-07 20:48:23 +0200

    get back NDF parsing test

--- test/NdfParsing.purs
+++ test/NdfParsing.purs
@@ -26,23 +26,25 @@ number 40 40 num-0
 <> pi-0 foo osc-0 bar
 -> osc-0 foo N 20.0
 ~> num-0 bar N 40.0
+. 20 30 pi-0
 """
 
 
 expectedNdf :: NdfFile
 expectedNdf =
     NdfFile.from "hydra" 0.1
-        [ C.MakeNode "osc" 40 60 "osc-0"
-        , C.MakeNode "osc" 40 60 "osc-1"
-        , C.MakeNode "pi" 20 20 "pi-0"
-        , C.MakeNode "number" 40 40 "num-0"
-        , C.Connect "pi-0" 0 "osc-0" 0
-        , C.Connect "num-0" 0 "osc-0" 1
-        , C.Send "osc-0" 0 "N 20.0"
-        , C.SendO "num-0" 0 "N 40.0"
-        , C.Connect_ "pi-0" "foo" "osc-0" "bar"
-        , C.Send_ "osc-0" "foo" "N 20.0"
-        , C.SendO_ "num-0" "bar" "N 40.0"
+        [ C.MakeNode (C.family "osc") (C.coord 40) (C.coord 60) (C.nodeId "osc-0")
+        , C.MakeNode (C.family "osc") (C.coord 40) (C.coord 60) (C.nodeId "osc-1")
+        , C.MakeNode (C.family "pi") (C.coord 20) (C.coord 20) (C.nodeId "pi-0")
+        , C.MakeNode (C.family "number") (C.coord 40) (C.coord 40) (C.nodeId "num-0")
+        , C.Connect (C.nodeId "pi-0") (C.outputIndex 0) (C.nodeId "osc-0") (C.inputIndex 0)
+        , C.Connect (C.nodeId "num-0") (C.outputIndex 0) (C.nodeId "osc-0") (C.inputIndex 1)
+        , C.Send (C.nodeId "osc-0") (C.inputIndex 0) (C.encodedValue "N 20.0")
+        , C.SendO (C.nodeId "num-0") (C.outputIndex 0) (C.encodedValue "N 40.0")
+        , C.Connect (C.nodeId "pi-0") (C.outputAlias "foo") (C.nodeId "osc-0") (C.inputAlias "bar")
+        , C.Send (C.nodeId "osc-0") (C.inputAlias "foo") (C.encodedValue "N 20.0")
+        , C.SendO (C.nodeId "num-0") (C.outputAlias "bar") (C.encodedValue "N 40.0")
+        , C.Move (C.nodeId "pi-0") (C.coord 20) (C.coord 30)
         ]
 
 
