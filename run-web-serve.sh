c7dbdf76725d3bc2709f58f0222a581b5825f271
Ulric Wilfred <shaman.sir@gmail.com>
2025-04-16 22:17:17 +0200

    disable `parcel` cache when running build & serve

--- run-web-serve.sh
+++ run-web-serve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 spago-legacy bundle-app -m Web.Main --to web/app.js
-parcel build web/app.html
-parcel serve ./web/app.html
+parcel build web/app.html --no-cache
+parcel serve ./web/app.html --no-cache
 # spago-legacy bundle-app --watch -m Web.Main --to web/app.js &
