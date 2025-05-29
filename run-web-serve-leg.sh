180c130d669ffd476f457b022f43c42a20b06320
Ulric Wilfred <shaman.sir@gmail.com>
2025-05-29 12:06:58 +0200

    configure specific `output.legacy` for `spago-legacy` + implement `starter.letter` node

--- run-web-serve-leg.sh
+++ run-web-serve-leg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-spago-legacy bundle-app -m Web.Main --to web/app.js
+spago-legacy bundle-app -m Web.Main --to web/app.js --purs-args "-o ./output.legacy"
 parcel build web/app.html --no-cache
 parcel serve ./web/app.html --no-cache
 # spago-legacy bundle-app --watch -m Web.Main --to web/app.js &
