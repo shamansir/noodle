68b39f2985e25178462448ad6fc46066dc7187e0
shaman.sir <shaman.sir@gmail.com>
2025-10-24 00:32:27 +0200

    Work on alternative `HelpText` approach

--- src/Front/Web/Components/HelpText.purs
+++ src/Front/Web/Components/HelpText.purs
@@ -2,12 +2,13 @@ module Web.Components.HelpText where
 
 import Prelude
 
-import Data.Maybe (Maybe(..))
+import Blessed.UI.Forms.Form.Event (FormEvent(..))
 import Data.Array (intersperse) as Array
-
+import Data.Maybe (Maybe(..))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Properties (style) as HHP
+import Web.Components.AppScreen.KeyboardLogic (Action(..)) as KL
 
 
 data Context
@@ -18,6 +19,170 @@ data Context
     | CreatingLink
     | DraggingNode
     | EnteringValue
+
+
+data Controller
+    = Mouse
+    | Keyboard
+
+
+data PossibleAction
+    = HideInterface
+    | ToggleSolidBackground
+    | ShowInterface
+    | LaunchCommandInput
+    | EnterCommand
+    | SpawnFromLibrary
+    | SelectFamilyToSpawn
+    | CreatePatch
+    | SelectPatch
+    | DragNode
+    | MoveNode
+    | ConfirmMovingNode
+    | CancelMovingNode
+    | FinishDraggingNode
+    | SelectNode
+    | SelectInletsOrOutlets
+    | SelectInlet
+    | SelectOutlet
+    | EditInletValue
+    | FinishEditingInletValue
+    | StartConnectingNodes
+    | FinishConnectingNodes
+    | CancelConnectingNodes
+    | RemoveLinks
+    | SelectMoreNodes
+    | DeselectNodes
+    | DeleteNode
+    | ChangeZoom
+    | ResetZoom
+    | HoverForDocumentation
+    | StepBackInKeyboardCombo
+    | ObserveKeyboardCombo
+
+
+helpText' :: Controller -> PossibleAction -> Array String
+helpText' controller = case _ of
+    HideInterface ->
+        [ "Hide the user interface using <space>, leaving only the canvas visible"
+        , "(and get it back by pressing <space> again)"
+        ]
+    ToggleSolidBackground ->
+        [ "Press <shift>+<space> to toggle solid background"
+        ]
+    ShowInterface ->
+        [ "Show the user interface using <space>"
+        ]
+    SpawnFromLibrary ->
+        case controller of
+            Mouse ->
+                [ "Spawn a node by clicking the desired family in the Library on the left"
+                , "You can scroll the list using mouse wheel or trackpad"
+                ]
+            Keyboard ->
+                [ "Use keyboard key <l> to focus on the Library"
+                ]
+    SelectFamilyToSpawn ->
+        [ "Press the index-key that is shown close to the family name and press <enter> to confirm"
+        ]
+    LaunchCommandInput ->
+        [ "Call command input by pressing <tab>" ]
+    EnterCommand ->
+        [ "Try typing the node family and pressing <enter>, for example `osc` will spawn the corresponding node"
+        , "You can also create custom nodes by typing something like `:: <a:Number -> b:Number -> c:Number> => <out>` or `:: <tuple> => <fst -> snd>`, just start with double-semicolon"
+        , "To close the input without applying, press <escape>"
+        , "To apply the command, press <enter> after entering it"
+        ]
+    CreatePatch ->
+        -- [ "Create a patch by selecting multiple nodes (hold <shift> and click on nodes), then press <p>"
+        -- ]
+        [ "Create a new patch by clicking (+) button in the patches bar"
+        ]
+    SelectPatch ->
+        [ "Select a patch by clicking on its name in the patches bar"
+        ]
+    DragNode ->
+        case controller of
+            Mouse ->
+                [ "To drag a node, hover over it and click the four-arrow button, then drag to the desired position and confirm by clicking again"
+                ]
+            Keyboard ->
+                [ ]
+    FinishDraggingNode ->
+        [ "Drag the node to the desired position and confirm by clicking the four-arrow button again"
+        ]
+    MoveNode ->
+        case controller of
+            Mouse ->
+                [ ]
+            Keyboard ->
+                [ "Move the node using arrow keys. To move faster, hold <shift> while pressing the arrow keys"
+                ]
+    ConfirmMovingNode ->
+        [ "Press <enter> to confirm the position"
+        ]
+    CancelMovingNode ->
+        [ "Press <escape> to move the node back to its previous place"
+        ]
+    SelectNode ->
+        case controller of
+            Mouse ->
+                [ "To select a node, hover over it and click on its body"
+                ]
+            Keyboard ->
+                [ "Press <n> to start selecting nodes"
+                ]
+    SelectInletsOrOutlets ->
+        [ "Press <i> to start selecting the particular inlet"
+        , "Press <o> to start selecting the particular outlet"
+        ]
+    SelectInlet ->
+        [ "Press the corresponding index-key that is shown close in the connector near the inlet name"
+        ]
+    SelectOutlet ->
+        [ "Press the corresponding index-key that is shown close in the connector near the outlet name"
+        ]
+    EditInletValue ->
+        [ "Changing value sends it to the inlet right away"
+        ]
+    FinishEditingInletValue ->
+        [ "To close the value editor, place <escape> or <enter>"
+        ]
+    StartConnectingNodes ->
+        []
+    FinishConnectingNodes ->
+        [ "To finish creating link, click on the inlet you want to connect to"
+        ]
+    CancelConnectingNodes ->
+        [ "To cancel creating link, click somewhere on the empty area or press <escape>"
+        ]
+    SelectMoreNodes ->
+        []
+    DeselectNodes ->
+        []
+    DeleteNode ->
+        [ "To remove some node, hover over it and click the cross button"
+        , "Alternatively, press <delete> or <x>"
+        ]
+    ChangeZoom ->
+        [ "Zoom with Shift+MouseWheel or <ctrl>+<+>/<->"
+        ]
+    ResetZoom ->
+        [ "Reset zoom by clicking (*) in the status bar"
+        ]
+    RemoveLinks ->
+        [ "To disconnect a link, just click somewhere on its shape"
+        ]
+    HoverForDocumentation ->
+        [ "Hover over inlets, outlets, nodes, to get the information in status bar"
+        ]
+    StepBackInKeyboardCombo ->
+        [ "Press <backspace> to step back in the keyboard combo sequence"
+        ]
+    ObserveKeyboardCombo ->
+        [ "You can track the keyboard combo sequence in the status bar"
+        ]
+
 
 
 helpText :: Context -> Array String
